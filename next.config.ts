5f46af29049120e0535eacd4ed542ab2d52a4786
danghuutrithuc <danghuutrithuc@gmail.com>
2026-05-13 19:56:12 +0700

    chore: add Cloudflare R2 remote patterns for producion

--- next.config.ts
+++ next.config.ts
@@ -8,12 +8,23 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "images.unsplash.com",
       },
+      // Local MinIO (development)
       {
         protocol: "http",
         hostname: "localhost",
         port: "9000",
         pathname: "/**",
-      }
+      },
+      {
+        protocol: "https",
+        hostname: "*.r2.cloudflarestorage.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "pub-*.r2.dev",
+        pathname: "/**",
+      },
     ],
   },
 };
